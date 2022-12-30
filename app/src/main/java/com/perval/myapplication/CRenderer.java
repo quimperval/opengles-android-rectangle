360a5af08e0a967463b87cefd309e0ae2261e915
quimperval <joaquinperezvalera@gmail.com>
2022-12-30 11:16:58 -0600

    Working example, rendering a red screen with a Renderer in an instance of GlSurfaceView

---
+++ app/src/main/java/com/perval/myapplication/CRenderer.java
@@ -0,0 +1,34 @@
+package com.perval.myapplication;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CRenderer implements GLSurfaceView.Renderer {
+    Context context;
+    public CRenderer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+        glViewport(0,0, i, i1);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
---
+++ app/src/main/java/com/perval/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.perval.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new CRenderer(this));
+        rendererSet = true;
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(rendererSet){
+            glSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(rendererSet){
+            glSurfaceView.onPause();
+        }
+    }
+
+}
